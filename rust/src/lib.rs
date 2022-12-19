40d2524def95220a24e96f842faac4b20798843c
gchak207 <>
2022-12-19 06:15:00 +0000

    Added tests for local events and sends

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,17 @@
 use std::collections::HashMap;
 
-fn get_timestamps(events : &[Vec<(i32, i32)>])->Vec<Vec<i32>> {
+pub fn get_timestamps(events : &[Vec<(i32, i32)>])->Vec<Vec<i32>> {
 	let mut timestamps : Vec<Vec<i32>> = Vec::new ();
-	let mut send_times
-		: HashMap<i32, i32> =
-			  HashMap::new (); // map from msgnum to timestamp of sender
   for
-	  i in 0..events.len() {
+	  _i in 0..events.len() {
 		  timestamps.push(Vec::new ());
 	  }
-  let mut next_eidx : Vec<i32> = vec ![0; events.len()];
+
+  let mut send_times
+	  : HashMap<i32, i32> =
+			HashMap::new (); // map from msgnum to timestamp of sender
+
+  let mut next_eidx : Vec<usize> = vec ![0; events.len()];
   let mut cur_ts : Vec<i32> = vec ![0; events.len()];
   let mut making_progress = true;
 
@@ -19,27 +21,30 @@ fn get_timestamps(events : &[Vec<(i32, i32)>])->Vec<Vec<i32>> {
 		  for (sidx, s_events)
 			  in events.iter().enumerate() {
 				  if
-					  next_eidx[sidx] >= int(s_events.len()) {
+					  next_eidx[sidx] >= s_events.len() {
 						  continue;
 					  }
-				  let evt = s_events[next_eidx[sidx]];
-				  match evt .0 {
-					  0 = > {
-						  cur_ts[sidx]++;
+				  let eidx = next_eidx[sidx];
+				  let evt : (i32, i32) = s_events[eidx];
+				  if
+					  evt .0 == 0 {
+						  cur_ts[sidx] = cur_ts[sidx] + 1;
 						  timestamps[sidx].push(cur_ts[sidx]);
+						  next_eidx[sidx] = next_eidx[sidx] + 1;
+						  making_progress = true;
 					  }
-					  , 1 = > {
-						  cur_ts[sidx]++;
+				  else if
+					  evt .0 == 1 {
+						  cur_ts[sidx] = cur_ts[sidx] + 1;
 						  timestamps[sidx].push(cur_ts[sidx]);
 						  send_times.insert(evt .0, cur_ts[sidx]);
+						  next_eidx[sidx] = next_eidx[sidx] + 1;
+						  making_progress = true;
 					  }
-					  ,
-						  2 = >
-						  {
-							  // check if msgnum received already
-						  },
-						  _ = > panic !("crash and burn"),
-				  }
+				  else if
+					  evt .0 == 2 {
+						  // check if msgnum received already
+					  }
 			  }
 	  }
   timestamps
--- rust/tests/integration_test.rs
+++ rust/tests/integration_test.rs
@@ -1,6 +1,25 @@
 use vector_clocks;
 
 #[test]
-fn empty_events() {
-	assert_eq !(0, vector_clocks::get_timestamps([]));
+fn local_events() {
+	let events = vec ![
+		vec ![ (0, 0), (0, 0), (0, 0) ],
+		vec ![ (0, 0), (0, 0) ],
+		vec ![(0, 0)]
+	];
+	let res = vector_clocks::get_timestamps(&events);
+	let expected = vec ![ vec ![ 1, 2, 3 ], vec ![ 1, 2 ], vec ![1] ];
+	assert_eq !(res, expected);
+}
+
+#[test]
+fn local_events_and_sends() {
+	let events = vec ![
+		vec ![ (0, 0), (1, 2), (1, 3) ],
+		vec ![ (0, 0), (1, 4) ],
+		vec ![(1, 7)]
+	];
+	let res = vector_clocks::get_timestamps(&events);
+	let expected = vec ![ vec ![ 1, 2, 3 ], vec ![ 1, 2 ], vec ![1] ];
+	assert_eq !(res, expected);
 }
